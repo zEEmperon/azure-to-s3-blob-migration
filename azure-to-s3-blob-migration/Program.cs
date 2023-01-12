f5b803190917bbdcbb87f64fa35d729895aedc10
zEEmperon <dan4ik2204.danya@gmail.com>
2023-01-12 19:33:48 +0200

    azure blob client

--- azure-to-s3-blob-migration/Program.cs
+++ azure-to-s3-blob-migration/Program.cs
@@ -1,9 +1,15 @@
-﻿namespace azure_to_s3_blob_migration;
+﻿using Azure.Storage.Blobs;
+
+namespace azure_to_s3_blob_migration;
 
 static class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello world");
+        var blobClient = GetBlobServiceClient();
+        Console.WriteLine(blobClient);
     }
+
+    private static BlobServiceClient GetBlobServiceClient()
+        => new (new Uri("http://127.0.0.1:10000/"));
 }
